c20ded9762a182a57bc26e7e3cb7dac588f536fe
vinisilveir4 <viniciussilverk@gmail.com>
2024-09-10 13:42:38 -0300

    projeto finalizado

--- js/informacoesTutorial.js
+++ js/informacoesTutorial.js
@@ -1,6 +1,7 @@
 const LINK_DOWNLOAD_WINDOWS = "https://dev.mysql.com/doc/refman/8.0/en/windows-installation.html";
 const LINK_DOWNLOAD_MAC = "https://dev.mysql.com/doc/refman/8.0/en/macos-installation.html";
 const LINK_DOWNLOAD_LINUX = "https://dev.mysql.com/doc/refman/8.0/en/linux-installation.html";
+const LINK_DOWNLOAD_XAMPP = "https://www.apachefriends.org/pt_br/download.html";
 
 let tutorialIniciado = false;
 
@@ -32,8 +33,21 @@ const tutorialMysql = [
 
             {
                 tituloConteudo: "Instalação no Windows",
-                descricaoConteudo: `A instalação do MySQL no Windows é bastante simples. Usando a instalação padrão, 
-                o Windows iniciará automaticamente o MySQL como um host local.`,
+                descricaoConteudo: `No Windows é bastante simples. Usando a instalação padrão, 
+                iniciará automaticamente o MySQL como um host local.`,
+                imagem: false
+            },
+            {
+                tituloConteudo: "Instalação no Linux",
+                descricaoConteudo: `Instale o MySQL via terminal com \`sudo apt-get install mysql-server\`. 
+                Inicie o serviço MySQL com \`sudo service mysql start\`.`,
+                imagem: false
+            },
+            {
+                tituloConteudo: "Verificação da Instalação",
+                descricaoConteudo: `
+                Após você pode verificar se o MySQL foi instalado corretamente executando o comando \`mysql --version\` no terminal. 
+                Isso irá mostrar a versão instalada do MySQL e garantir que está tudo pronto.`,
                 imagem: false
             }
         ],
@@ -42,19 +56,132 @@ const tutorialMysql = [
     },
     {
         id: 2,
-        tituloEtapa: "Configuração",
+        tituloEtapa: "Configurando Conexão",
         descricaoEtapa: `
-        Texto...`,
+        Após a instalação do MySQL, será necessário configurar uma conexão e, para isso, vamos utilizar o 
+        <a target="_blank" class="link-opacity-100 estilo-link" href="${LINK_DOWNLOAD_XAMPP}">XAMPP</a> 
+        para gerenciamento.`,
         conteudo: [
             {
-                tituloConteudo: "",
-                descricaoConteudo:``,
-                imagem: ""
+                tituloConteudo: "Instalação do XAMPP",
+                descricaoConteudo: `
+           O XAMPP é uma opção rápida para rodar o serviço MySQL junto com o Apache e PHP em um ambiente local. 
+           Faça o download e siga as instruções de instalação.`,
+                imagem: "./imagens/xampp.png"
+            },
+            {
+                tituloConteudo: "Criar uma conexão MySQL",
+                descricaoConteudo: `
+                Abra o MySQL Workbench e, no painel de "MySQL Connections", clique no ícone de "+" para adicionar uma nova conexão. 
+                Preencha os campos como "Hostname" (localhost) e "Port" (3306) que é a porta padrão do MySQL.
+                O "Username" geralmente será 'root', e insira a senha que foi configurada durante a instalação.
+                `,
+                imagem: "./imagens/conexao.png"
             }
         ],
         possiveisErros: false,
         status: Status.NAO_CONCLUIDO
+    },
+    {
+        id: 3,
+        tituloEtapa: "Criando Database",
+        descricaoEtapa: `Após configurar a conexão, o próximo passo é criar um banco de dados.`,
+
+        conteudo: [
+
+            {
+                tituloConteudo: "Criar Database",
+                descricaoConteudo: `
+                Coloque o comando "<spam class="estilo-sql">CREATE DATABASE</spam> nome_do_banco_de_dados;" (nome não pode possuir acentos ou espaço na query de conexão) e control + enter para executar.`,
+                imagem: "./imagens/criacao-database.png"
+            },
+            {
+                tituloConteudo: "Verificar DBs",
+                descricaoConteudo: `Após criar o banco de dados, execute "<spam class="estilo-sql">SHOW DATABASES;</spam>" para listar todos os bancos de dados criados. 
+`,
+                imagem: false
+            }
+        ],
+        status: Status.NAO_CONCLUIDO
+
+    },
+    {
+        id: 4,
+        tituloEtapa: "Primeira Tabela",
+        descricaoEtapa: `Um banco de dados (ou database) é onde você armazena as informações, 
+        e as tabelas são estruturas dentro do banco 
+        de dados que organizam esses dados em linhas e colunas. 
+        Cada tabela é como uma planilha, onde as colunas representam os campos (como nome, idade, data) 
+        e as linhas são os registros dos dados.`,
+        conteudo: [
+            {
+                tituloConteudo: "Selecionar sua Database",
+                descricaoConteudo: `Atualize a lista de "Schemas" e procure sua database criada e de dois cliques para seleciona-la
+                ou o comando "<spam class="estilo-sql">USE</spam> nome_do_banco_de_dados;".`,
+                imagem: false
+            },
+
+            {
+                tituloConteudo: "Criando tabela",
+                descricaoConteudo: `Para criarmos uma tabela, usamos o comando 
+                "<spam class="estilo-sql">CREATE TABLE</spam> nome_da_tabela ();" <br>
+                Ex: <br>
+                <spam class="estilo-sql">CREATE TABLE</spam> cliente (
+                    id <spam class="estilo-sql">int primary key auto_increment</spam>
+                ); <br>
+                Após isso control + enter, para ver sua tabela criada, atualize novamente e abra o "tables" clicando na seta.`,
+                imagem: "./imagens/criacao-tabela.png"
+            },
+
+            {
+                tituloConteudo: "Tipos de dados",
+                descricaoConteudo: `
+                <br>
+                <spam class="estilo-sql">Int</spam>, <<spam class="estilo-sql">varchar</spam>(?) e <spam class="estilo-sql">date</spam> - Tipos de dados que serão registrados; <br>
+                Int - Números inteiros; <br>
+                <spam class="estilo-sql">Varchar</spam>(?)- Registra textos, é preciso colocar o numero maximo de caracteres que ira conter entre os parenteses; <br>
+                <spam class="estilo-sql">Date</spam> - Registra apenas dia, mes e ano, para adicinar dado nela é preciso escrever 'YYYY-MM-dd'(Ano-mes-dia); <br>
+                <spam class="estilo-sql">Primary Key</spam> - É a chave primaria desta tabela, ela é importante para termos certeza que cada registo é unico na tabela; <br>
+                <spam class="estilo-sql">Auto_increment</spam> - Ela auto incrementa a cada registro, assim não a deixando nula e também não precisando ser definida a cada insert; <br>
+                Assim como no nome da Database, o nome das tabelas e dados não podem possuir espaço ou assentos;`,
+                imagem: "./imagens/explicacao-tabela.png"
+            }
+        ],
+        status: Status.NAO_CONCLUIDO
+
+    },
+    {
+        id: 5,
+        tituloEtapa: "Comandos",
+        descricaoEtapa: `Uma vez com sua tabela criada, você pode executar diversos comandos para gerenciar os dados.`,
+        conteudo: [
+            {
+                tituloConteudo: "Ver informações da tabela (SELECT)",
+                descricaoConteudo: `Para podermos ver as informações registradas usamos o comando "<spam class="estilo-sql">SELECT</spam> * <spam class="estilo-sql">FROM</spam> tabela;" assim vemos os dados salvos.`,
+                imagem: "./imagens/select-tabela.png"
+            },
+            {
+                tituloConteudo: "Inserir registros na tabela (INSERT)",
+                descricaoConteudo: `Para registrar informações na tabela usamos o comando "<spam class="estilo-sql">INSERT INTO</spam> tabela (atributo1, atributo2, ...) <spam class="estilo-sql">VALUES</spam> (valor1, valor2, ...);"
+                Para valores de texto, insira entre aspas simples ('').`,
+                imagem: "./imagens/insert-tabela.png"
+            },
+            {
+                tituloConteudo: "Atualizar informações de um registro (UPDATE)",
+                descricaoConteudo: `Utilizamos o comando, "<spam class="estilo-sql">UPDATE</spam> tabela <spam class="estilo-sql">SET</spam> dado_atualizar = registro_atualizado <spam class="estilo-sql">WHERE</spam> dado = registro_antigo;"`,
+                imagem: "./imagens/update-tabela.png"
+            },
+            {
+                tituloConteudo: "Deletar registros de uma tabela (DELETE)",
+                descricaoConteudo: `Utilizamos o comando, "<spam class="estilo-sql">DELETE FROM</spam> tabela <spam class="estilo-sql">WHERE</spam> dado_excluido = registro;".
+                Somente os registros que atendem à condição serão removidos.`,
+                imagem: "./imagens/delete-tabela.png"
+            }
+        ],
+        status: Status.NAO_CONCLUIDO
+
     }
+
 ];
 
 // export {Status, tutorialMysql};
--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ const areaTutorial = document.querySelector("#accordionTutorial");
 const botaoIniciar = document.querySelector("#botao-iniciar");
 console.log(areaTutorial)
 
-
 // nos cards o id do accordion e botao é diferente para cada ent não da pra setar um valor default
 // conforme adicionando etapa no tutorial adicionar novo id seguindo a ordem
 const IDS_ACCORDION = [
     "collapseOne",
     "collapseTwo",
+    "collapseThree",
+    "collapseFour",
+    "collapseFive"
 ];
 
 document.addEventListener("DOMContentLoaded", () => renderizarTutorial());
@@ -34,32 +36,34 @@ function renderizarTutorial() {
 
 function iniciarTutorial() {
     liberarEtapa(tutorialMysql[0].id);
-    const { etapa } = selectItem(1);
+    const { etapa, btn, accordioBody } = selectItem(1);
     atualizarStatus(etapa, Status.EM_PROGRESSO);
 
-    botaoIniciar.setAttribute("disabled", "true");
-    tutorialIniciado = true;
-};
-
-function liberarEtapa(id) {
-    const { etapa, btn, accordioBody } = selectItem(id);
-
-    etapa.classList.remove("efeito-desativado");
-    btn.removeAttribute("disabled");
     btn.classList.remove("collapsed");
     btn.setAttribute("area-expanded", "true")
 
     accordioBody.classList.add("show");
-    atualizarStatus(etapa, Status.EM_PROGRESSO)
+    botaoIniciar.setAttribute("disabled", "true");
+
+    tutorialIniciado = true;
+};
+
+function liberarEtapa(id) {
+    const { etapa, btn } = selectItem(id);
+
+    etapa.classList.remove("efeito-desativado");
+    btn.removeAttribute("disabled");
+    atualizarStatus(etapa, Status.EM_PROGRESSO);
 };
 
 function fecharEtapa(id) {
-    const { btn, accordioBody } = selectItem(id);
+    const { etapa, btn, accordioBody } = selectItem(id);
 
     btn.classList.add("collapsed");
     btn.setAttribute("area-expanded", "false")
 
     accordioBody.classList.remove("show");
+    atualizarStatus(etapa, Status.CONCLUIDO);
 };
 
 function selectItem(id) {
@@ -77,10 +81,11 @@ function criarAccordion(item, index) {
     const accordionButton = document.createElement("button");
     const accordionBody = document.createElement("div");
     const textoStatus = document.createElement("p");
+    const textoDivStatus = document.createElement("p");
     const textoTitulo = document.createElement("p");
     const collapseDiv = document.createElement("div");
     const divDivisao = document.createElement("div");
-
+    const divStatus = document.createElement("div");
 
     accordionItem.classList.add("accordion-item")
     accordionItem.setAttribute("id-tutorial", tutorialMysql[index].id);
@@ -92,11 +97,20 @@ function criarAccordion(item, index) {
     accordionButton.setAttribute("data-bs-toggle", "collapse");
     accordionButton.setAttribute("aria-expanded", "true");
     accordionButton.setAttribute("aria-controls", IDS_ACCORDION[index]);
-    accordionButton.setAttribute("data-bs-target", "#" + IDS_ACCORDION[index])
+    accordionButton.setAttribute("data-bs-target", "#" + IDS_ACCORDION[index]);
 
-    textoStatus.classList.add("m-0", "texto-status", "texto-generico");
-    textoStatus.style.width = "22%";
-    textoStatus.textContent = `Status: ${item.status}`;
+    divStatus.classList.add("d-flex", "align-items-center", "gap-1");
+    divStatus.style.width = "150px";
+    textoDivStatus.classList.add("m-0", "texto-generico");
+    textoDivStatus.textContent = "Status:";
+    const corTexto = item.status == Status.NAO_CONCLUIDO ?
+        "txt-status-nao-concluido" : item.status == Status.EM_PROGRESSO ?
+            "text-primary" : ".text-success";
+
+    textoStatus.classList.add("m-0", "texto-status", corTexto);
+    textoStatus.style.fontSize = "15.4px";
+    textoStatus.style.whiteSpace = "nowrap";
+    textoStatus.textContent = `${item.status}`;
 
     textoTitulo.classList.add("m-0", "fw-bold", "texto-titulo", "texto-generico");
     textoTitulo.textContent = `${item.tituloEtapa}`;
@@ -107,7 +121,7 @@ function criarAccordion(item, index) {
     collapseDiv.setAttribute("id", IDS_ACCORDION[index]);
 
     accordionBody.classList.add("accordion-body", "container-fluid", "row", "gap-4");
-    accordionBody.innerHTML = bodyAccordion(item.descricaoEtapa, item.conteudo, item.status);
+    accordionBody.innerHTML = bodyAccordion(item.descricaoEtapa, item.conteudo, item.status, item.id);
 
     // verifica se o tutorial foi iniciado ou status
     // se não bloqueia todos card e ativa/desativa o botao
@@ -123,6 +137,9 @@ function criarAccordion(item, index) {
     accordionItem.appendChild(collapseDiv);
     accordionHeader.appendChild(accordionButton);
     accordionButton.appendChild(textoStatus);
+    accordionButton.appendChild(divStatus);
+    divStatus.appendChild(textoDivStatus);
+    divStatus.appendChild(textoStatus);
     accordionButton.appendChild(divDivisao);
     accordionButton.appendChild(textoTitulo);
     collapseDiv.appendChild(accordionBody);
@@ -130,7 +147,7 @@ function criarAccordion(item, index) {
     return accordionItem;
 };
 
-function bodyAccordion(descricao, conteudo, status) {
+function bodyAccordion(descricao, conteudo, status, id) {
     let conteudoBody = `
         <p class="m-0 col-12 texto-generico">${descricao}</p>
     `;
@@ -141,14 +158,18 @@ function bodyAccordion(descricao, conteudo, status) {
             <p class="m-0 col-12"><b>${item.tituloConteudo}:</b>
             ${item.descricaoConteudo}
             </p>
-            <img src="${item.imagem}" class="img-fluid" alt="...">
+            <div class="col-12">
+            ${!!item.imagem ? `<img src="${item.imagem}" class="img-fluid img-visu" onclick="visualizarImg(this)" alt="...">
+            <p class="m-0 text-secondary" style="font-size: 12.8px"><small>Clique na imagem para melhor visualização.</small> </p>`
+            : ""}
+            </div>
             </div>
         `;
     });
 
     let btnProximo = `
-    <button type="button" class="btn btn-primary ms-3" ${status == Status.CONCLUIDO ? "disabled" : ""} 
-    onclick="concluirEtapa(this)" style="width:max-content">Proximo</button>
+    <button type="button" class="btn btn-primary ms-3 btn-sm" ${status == Status.CONCLUIDO ? "disabled" : ""} 
+    onclick="concluirEtapa(this)" style="width:max-content">${tutorialMysql[tutorialMysql.length - 1].id == id ? "Concluir tutorial" : "Proximo"}</button>
     `;
 
     return conteudoBody + btnProximo;
@@ -163,13 +184,13 @@ function concluirEtapa(botao) {
 
     const id = getId(elmPai);
 
-    if (tutorialMysql[tutorialMysql.length - 1].id == id) 
+    if (tutorialMysql[tutorialMysql.length - 1].id == id)
         finalizarTutorial();
-    else 
+    else
         liberarEtapa(id + 1);
-    
+
     fecharEtapa(id);
-    atualizarStatus(elmPai, Status.CONCLUIDO);
+    // atualizarStatus(elmPai, Status.CONCLUIDO);
     botao.setAttribute("disabled", "true");
     atualizarProgresso(tutorialMysql);
 };
@@ -179,7 +200,15 @@ function atualizarStatus(elemento, status) {
     const txtStatus = elemento.querySelector(".texto-status");
 
     tutorialMysql[tutorialMysql.findIndex(i => i.id == id)].status = status;
-    txtStatus.textContent = `Status: ${status}`;
+    txtStatus.textContent = `${status}`;
+
+    if (status == Status.EM_PROGRESSO) {
+        txtStatus.classList.remove("txt-status-nao-concluido");
+        txtStatus.classList.add("text-primary");
+    } else {
+        txtStatus.classList.remove("text-primary");
+        txtStatus.classList.add("text-success");
+    }
 };
 
 function atualizarProgresso(nTutorial) {
@@ -195,9 +224,27 @@ function atualizarProgresso(nTutorial) {
     tProgresso.textContent = `Seu progresso: ${calcProgresso}%`;
 };
 
+function visualizarImg(elementoImg) {
+    const divImg = document.querySelector(".div-full-img");
+    const img = divImg.querySelector(".img-conteudo");
+    img.src = elementoImg.getAttribute("src");
+
+    divImg.style.display = "flex";
+};
+
+function fecharImg(elementoBtn) {
+    const elmPai = elementoBtn
+        .parentElement
+        .parentElement;
+
+    const img = elmPai.querySelector(".img-conteudo");
+    img.src = "";
+    elmPai.style.display = "none";
+};
+
 
 function finalizarTutorial() {
-    alert("Parabéns voce concluiu");
+    alert("Parabéns, voce concluiu o tutorial!");
 };
 
 {/*  
